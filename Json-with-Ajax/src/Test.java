3cb4e48ef671aee2969e4fd06717bfbe54b8a111
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-18 21:24:06 +0530

    Added initial project

---
+++ Json-with-Ajax/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Gathsara
+ * created : 8/18/2023 -- 9:20 PM
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("testing");
+    }
+}
